6a5482aee2db595d021fcb147e423f6e03acce99
CHINOBv <retrochanel97@gmail.com>
2021-01-06 17:58:24 -0600

    Now i get all emails

--- src/index.js
+++ src/index.js
@@ -1,6 +1,43 @@
 const puppeteer = require("puppeteer");
 
+let emailsDatas = [];
+const iterateEmails = async (page) => {
+  await page.waitForTimeout(2000);
+
+  let emails = await page.$eval("#messagelist > tbody", (element) => {
+    let emailsOfPage = [];
+    //Aqui se sacan los emails de la tabla, solo las filas
+    const emails = element.childNodes;
+
+    //const emailAdress
+    emails.forEach((element) => {
+      let emailAddress;
+      let emailFormTo;
+      try {
+        //Se itera cada email que se encuentre y lo muestra en la consola, luego de esto sigue pasarlos a bambino
+        emailAddress = element.childNodes[6].lastChild.childNodes[0].title;
+        emailFormTo =
+          element.childNodes[6].lastChild.childNodes[0].childNodes[0].data;
+      } catch (error) {
+        console.log(error);
+      }
+      let emailInfo = {
+        emailAddress,
+        emailFormTo,
+      };
+
+      //console.log(emailInfo);
+      emailsOfPage.push(emailInfo);
+    });
+    console.log("=====PAGINA=====");
+    return emailsOfPage;
+  });
+
+  emailsDatas.push(emails);
+};
+
 async function Start() {
+  //Se abre el navegador
   const browser = await puppeteer.launch({
     headless: false,
   });
@@ -15,12 +52,14 @@ async function Start() {
   );
 
   //Login
+  //Se inicia sesion
   await page.waitForSelector("#user");
   await page.type("#user", "maximo84");
   await page.type("#pass", "Sarabia144");
   await page.click("#login_submit");
 
   //Select Account
+  //Se cambia a la cuenta de Fumigaciones
 
   await page.waitForSelector("#ddlAccounts_chosen");
   await page.waitForTimeout(1500);
@@ -32,6 +71,7 @@ async function Start() {
   await page.click("#ddlAccounts_chosen > div > ul > li:nth-child(1)");
 
   //Entry to Emails
+  //Entra al listado de emails en el hosting
 
   await page.waitForSelector("#item_email_accounts");
   await page.click("#item_email_accounts");
@@ -48,25 +88,41 @@ async function Start() {
 
   //Configure Emails view
 
-  
-
   //Search Email
 
+  //Se hace la busqueda entre los correos
   await emailPage.waitForSelector("#quicksearchbox");
   await emailPage.type("#quicksearchbox", "fumigaciones.com:");
   await emailPage.click("#searchmenulink");
   await emailPage.click("#searchmenu > div > a");
 
-  await emailPage.waitForTimeout(2000);
-  //Get Emails
+  //Get Emails Page 1
+  //Se obtiene el cuerpo de la tabla que contiene los emials
   await emailPage.waitForSelector("#messagelist > tbody");
-  await emailPage.$eval("#messagelist > tbody", (element) => {
-    const emails = element.childNodes;
+  await iterateEmails(emailPage);
 
-    emails.forEach((element) =>
-      console.log(element.childNodes[6].lastChild.childNodes[0].title)
-    );
+  //Next Page 2
+  await emailPage.click("#rcmbtn133");
+
+  await iterateEmails(emailPage);
+
+  //Next Page 3
+  await emailPage.click("#rcmbtn133");
+  await iterateEmails(emailPage);
+
+  //Next Page 4
+  await emailPage.click("#rcmbtn133");
+  await iterateEmails(emailPage);
+
+  let c = 0;
+  emailsDatas.forEach((e) => {
+    e.forEach(e =>{
+
+      c = c + 1;
+      console.log(c);
+    })
   });
+  console.log(c);
 }
 
 Start();
